7f228e349945a4dbcf24c70e13e12ada75e708f7
Frank <841290355@qq.com>
2023-06-22 22:54:07 +0800

    feat: BaseActivity

--- app/src/main/java/com/ixuea/courses/android_first/MainActivity.java
+++ app/src/main/java/com/ixuea/courses/android_first/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ixuea.courses.android_first.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
